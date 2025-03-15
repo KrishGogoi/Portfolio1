f6738fe37632db65411ca5035875ef4fc014c6f0
Krishna Gogoi <183848934+KrishGogoi@users.noreply.github.com>
2025-03-15 23:10:42 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./about.css";
 import AboutImg from "../../assets/me_icon.jpeg";
-import CV from "../../assets/Krishna_Resume.pdf";
+import CV from "../../assets/KrishnaGogoi_Resume.pdf";
 import Info from './Info';
 
 const About = () => {
